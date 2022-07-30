0c8038d93933714db00da116bc22ef328fb3b30a
LeeJaesung2 <dlwotjd9909@naver.com>
2022-07-30 16:37:00 +0900

    nav done

--- thebrack/settings.py
+++ thebrack/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[ os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS=[ os.path.join(BASE_DIR,'static'),]
