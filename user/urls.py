8986a463a06f5cab444d47435bec92615e323cf3
LeeJaesung2 <dlwotjd9909@naver.com>
2022-08-22 17:14:09 +0900

    kakao login

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,6 @@ from .import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('kakao/login/', views.KakaoLoginView.as_view(), name="kakaologin"),
+    path('kakao/signup/',views.login_api, name="signup"),
     path('kakao/login/callback/', views.KakaoCallbackView.as_view()),
 ]
